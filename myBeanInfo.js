138fcd3d332df6d786049f4a6c44030e9b4ac261
neozeg <neozeg@qq.com>
2022-03-09 11:08:41 +0800

    main

--- myBeanInfo.js
+++ myBeanInfo.js
@@ -1,3 +1,21 @@
+/*
+==========================Quantumultx=========================
+[task_local]
+#jd免费水果
+5 * * * * https://gitee.com/lxk0301/jd_scripts/raw/master/jd_fruit.js, tag=东东农场, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jdnc.png, enabled=true
+=========================Loon=============================
+[Script]
+cron "5 * * * *" script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_fruit.js,tag=东东农场
+
+=========================Surge============================
+东东农场 = type=cron,cronexp="5 * * * *",wake-system=1,timeout=3600,script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_fruit.js
+
+=========================小火箭===========================
+东东农场 = type=cron,script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_fruit.js, cronexpr="5 * * * *", timeout=3600, enable=true
+
+jd免费水果 搬的https://github.com/liuxiaoyucc/jd-helper/blob/a6f275d9785748014fc6cca821e58427162e9336/fruit/fruit.js
+*/
+
 const $ = new Env('my京东详情');
 const notify = $.isNode() ? require('./sendNotify') : '';
 const mqttMsg = $.isNode() ? require(`./sendMqttMsg`):'';
